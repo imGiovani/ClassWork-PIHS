0c2840084d9d393d4ed9af9579de6d1915c650a7
Giovani Oliveira <60044063+imGiovani@users.noreply.github.com>
2025-06-13 11:30:18 -0300

    FixedScript

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ for i in range(len(strings)):
     distance = py_levenshtein(string_one, string_two)
     print('Aplying Levenshtein Distance between: ' + string_one + ' & ' + string_two + ' it results in: ' + str(distance))
 
-print('======Distancia de Hamming:======')
+print('.:. Strings distance aplying Hamming .:.')
 strings = ['Erick Errik', 'Jonn John', 'Peddro Pascal', 'Giovani Oliveir']
 for i in range(len(strings)):
     string_one, string_two = strings[i].split(' ')
